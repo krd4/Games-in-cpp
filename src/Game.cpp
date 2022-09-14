05ac1f2c5f9082b1e03d51bc88afd7f1b34f4e1a
kryousuke <k000ryousuke@gmail.com>
2022-09-14 20:42:14 +0900

    Add Game class and Main file

---
+++ src/Game.cpp
@@ -0,0 +1,72 @@
+#include "Game.h"
+
+Game::Game()
+:mWindow(nullptr)
+,mIsRunning(true)
+{
+	
+}
+
+bool Game::Initialize()
+{
+    //SDL_INIT_VIDEO is set for creating a window
+    int sdlResult = SDL_Init(SDL_INIT_VIDEO);
+    // sdlResult == 0 indicates initialization succeed; otherwise there is something failed.
+    if(sdlResult !=  0)
+    {
+        SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
+        return false;
+    }
+
+    mWindow = SDL_CreateWindow(
+        "title",
+        100,
+        100,
+        1024,
+        768,
+        0 // Flags (0 for no flags set)
+    );
+
+    if(!mWindow)
+    {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+        return false;
+    }
+
+    return true;
+}
+
+
+void Game::Shutdown()
+{
+    SDL_DestroyWindow(mWindow);
+    SDL_Quit();
+}
+
+void Game::RunLoop()
+{
+    while (mIsRunning)
+    {
+        ProcessInput();
+    }
+}
+
+void Game::ProcessInput() 
+{
+    SDL_Event event;
+    while(SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                mIsRunning = false;
+                break;
+        }
+    }
+
+    const Uint8* state = SDL_GetKeyboardState(NULL);
+    if(state[SDL_SCANCODE_ESCAPE])
+    {
+        mIsRunning = false;
+    }
+}
